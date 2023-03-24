8ae440cbc54095278dbf9e9b14871b61936b88e7
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2023-03-23 17:13:39 -0700

    Hack DB tags for Tempo ServiceGraph

--- dog-service/src/main/java/com/example/dogservice/actuator/ActuatorConfiguration.java
+++ dog-service/src/main/java/com/example/dogservice/actuator/ActuatorConfiguration.java
@@ -3,6 +3,7 @@ package com.example.dogservice.actuator;
 import io.micrometer.common.KeyValue;
 import io.micrometer.observation.ObservationFilter;
 import io.micrometer.observation.ObservationPredicate;
+import net.ttddyy.observation.tracing.DataSourceBaseContext;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,6 +29,17 @@ public class ActuatorConfiguration {
 			if (context.getError() != null) {
 				context.addHighCardinalityKeyValue(KeyValue.of("error", "true"));
 				context.addHighCardinalityKeyValue(KeyValue.of("errorMessage", context.getError().getMessage()));
+			}
+			return context;
+		};
+	}
+
+	@Bean
+	ObservationFilter tempoServiceGraphFilter() {
+		// TODO: remove this once Tempo is fixed: https://github.com/grafana/tempo/issues/2212
+		return context -> {
+			if (context instanceof DataSourceBaseContext dataSourceContext) {
+				context.addHighCardinalityKeyValue(KeyValue.of("db.name", dataSourceContext.getRemoteServiceName()));
 			}
 			return context;
 		};
